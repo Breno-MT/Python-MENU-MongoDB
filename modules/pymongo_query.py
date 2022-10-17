22453a184fabe62098936b5a11b2688af2c3dd85
Breno-MT <hentgenp@gmail.com>
2022-10-17 15:02:28 -0300

    [BRENO] refactor: query function

--- modules/pymongo_query.py
+++ modules/pymongo_query.py
@@ -8,8 +8,7 @@ def query_all():
     query_all = collection_name.find()
     query_df = DataFrame(query_all)
 
-    print(f"""User(s): \n
-            {query_df}""")
+    print(f"""User(s): \n {query_df}""")
 
 
 def query_by_name(name):
@@ -17,9 +16,13 @@ def query_by_name(name):
     collection_name = dbname["users"]
 
     query_name = collection_name.find({"name": {"$regex": f"{name}"}})
+    query_df = DataFrame(query_name)
 
-    print(f"""User(s) found: \n
-        {query_name}""")
+
+    if len(query_df) > 0:
+        print(f"""User(s) found: \n {query_df}""")
     
-    print("If you didn't found your user, don't worry, try again.")
+    if len(query_df) <= 0:
+        print("No Users found! :(")
+        print("If you didn't found your user, don't worry, try again.")
 
